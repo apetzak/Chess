d716722ce2c83a665231c5d09be024d5db02bf63
Alec <apetzak1@gmail.com>
2019-12-02 23:56:20 -0500

    Initial commit
    Programmatically generate game board and set default images
    Selected button gets highlighted
    New button resets board

---
+++ scripts.js
@@ -0,0 +1,25 @@
+//var el = document.getElementById('element');
+//document.getElementsByTagName('body')[0].innerHTML = '<button style="background-color: #D4C4C4; left: 160px; top: 100px;"></button>';
+
+//object.onclick = function(){myScript};
+
+//var x = document.createElement("BUTTON");
+//body.appe
+
+//document.getElementsByTagName('body')[0].innerHTML = '<button id="close-image" style="background-color: #D4C4C4; left: 560px; top: 100px;"><img src="rook.png"></button>';
+
+//var el = 
+//el.innerHTML = '<p><a id="clickme" href="#">Click me</a></p>';
+
+
+/*
+document.getElementById('clickme').onclick = function (e) {
+    e.preventDefault();
+    document.body.innerHTML = '<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;"></div>';
+}
+
+function createButtons() {
+    document.body.innerHTML = '<button style="background-color: #D4C4C4; left: 160px; top: 100px;"></button>';
+  }
+
+  */
